992ffa0634a663373b72af04312ab92ae9ea60b6
imhawool <ektmfrlehdrh@gmail.com>
2021-05-21 16:18:18 +0900

    Create 정수삼각형.java

---
+++ Lv3/정수삼각형.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int[][] triangle) {
+        int answer = 0;
+        int[][] dp = new int[triangle.length][triangle.length];
+
+        dp[0][0] = triangle[0][0];
+
+        for(int i = 1; i < triangle.length; i++){
+
+            dp[i][0] = triangle[i][0] + dp[i-1][0];
+            dp[i][i] = triangle[i][i] + dp[i-1][i-1];
+
+            for(int j = 1; j < i; j++){
+                dp[i][j] = triangle[i][j] + Math.max(dp[i-1][j-1],dp[i-1][j]);
+            }
+        }
+
+        answer = dp[triangle.length - 1][0];
+        for(int i = 1; i < triangle.length; i++){
+            if(answer < dp[triangle.length - 1][i]){
+                answer = dp[triangle.length - 1][i];
+            }
+        }
+        return answer;
+    }
+}
