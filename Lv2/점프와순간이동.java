b32b6d48bea585225720ff0c076021c0b7ce0410
imhawool <ektmfrlehdrh@gmail.com>
2021-06-02 10:35:28 +0900

    Create 점프와순간이동.java

---
+++ Lv2/점프와순간이동.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int solution(int n) {
+      int ans = 1;
+		
+      while(n > 1){
+
+        if(n%2 == 0){
+          n /= 2;
+        }else{
+          n -= 1;
+          ans++;
+        }
+      }
+
+          return ans;
+      }
+}
