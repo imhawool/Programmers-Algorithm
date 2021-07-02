2a0c8445f135414a2e58d07be909f8071a701fab
imhawool <ektmfrlehdrh@gmail.com>
2021-07-02 14:34:45 +0900

    Update 등굣길.java

--- Lv3/등굣길.java
+++ Lv3/등굣길.java
@@ -8,19 +8,19 @@ public class Solution {
 
     street[0][0] = 1;
 
-    for (int i = 0; i < n; i++) { // 시작점은 1로 저장
+    for (int i = 0; i < n; i++) {
       for (int j = 0; j < m; j++) {
 
-        if(street[i][j] == -1) { // 웅덩이면 0으로 바꾸고 continue
+        if(street[i][j] == -1) {
           street[i][j] = 0;
           continue;
         }
 
         if(i != 0)
-          street[i][j] += street[i - 1][j] % 1000000007; // 숫자가 이 값을 초과할 수 있기 때문에 계산 과정에서 나머지 구하기
+          street[i][j] += street[i - 1][j] % 1000000007;
 
         if(j != 0)
-          street[i][j] += street[i][j - 1] % 1000000007; // 왼쪽
+          street[i][j] += street[i][j - 1] % 1000000007;
       }
     }
 
