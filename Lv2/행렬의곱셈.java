f42d5d463e572636d1280fe9dc1156d05bef8330
imhawool <ektmfrlehdrh@gmail.com>
2021-11-07 09:50:51 +0900

    Create 행렬의곱셈.java

---
+++ Lv2/행렬의곱셈.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+        
+        for(int i = 0; i < arr1.length; i++){
+            for(int k = 0; k < arr2[0].length; k++){
+                for(int j = 0; j < arr1[0].length; j++){
+                    answer[i][k] += arr1[i][j] * arr2[j][k];
+                }    
+            }            
+        }
+        return answer;
+    }
+}
