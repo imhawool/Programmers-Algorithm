cb00242e56bf6265b3a36d496eccab039182fef4
imhawool <ektmfrlehdrh@gmail.com>
2022-02-06 11:24:47 +0900

    Create 전력망을둘로나누기.java

---
+++ Lv2/전력망을둘로나누기.java
@@ -0,0 +1,30 @@
+class Solution {
+    int N, min;
+    int[][] map;
+    int[] vst;
+    public int solution(int n, int[][] wires) {
+        N = n;
+        min = n;
+        map = new int[n+1][n+1];
+        vst = new int[n+1];
+        for(int[] wire : wires) {
+            int a = wire[0], b = wire[1];
+            map[a][b] = map[b][a] = 1;
+        }
+        dfs(1);
+        return min;
+    }
+    
+    public int dfs(int n){
+        vst[n] = 1;
+        int child = 1;
+        for(int i = 1; i <= N; i++) {
+            if(vst[i] == 0 && map[n][i] == 1) {
+                vst[i] = 1;
+                child += dfs(i);
+            }
+        }
+        min = Math.min(min, Math.abs(child - (N - child)));
+        return child;
+    }
+}
